a80cc5e81f9ed3870dd2a3b31411f78d4e9e445b
Shelley0207 <164703253+Shelley0207@users.noreply.github.com>
2024-03-31 11:37:53 +0800

    Add files via upload

---
+++ TurnTable-master/Form1.cs
@@ -0,0 +1,107 @@
+namespace TurnTableDemo
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 偏移量
+        /// </summary>
+        private float roffset = 20;
+
+        /// <summary>
+        /// 大转盘旋转计时器
+        /// </summary>
+        private System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+        /// <summary>
+        /// 设置旋转力度时用到的计时器
+        /// </summary>
+        private System.Windows.Forms.Timer t1 = new System.Windows.Forms.Timer();
+
+        private readonly TurnTable Turn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            t.Tick += T_Tick;
+            t1.Tick += T1_Tick;
+            Turn = new(pictureBox1.Handle, new List<string>() { });
+            Turn.Diameter = 800;
+            richTextBox1_Validated(new object(), new EventArgs());
+
+        }
+
+        private int step = 1;
+        private void T1_Tick(object? sender, EventArgs e)
+        {
+            if (progressBar1.Value <= progressBar1.Minimum) step = 1;
+            if (progressBar1.Value >= progressBar1.Maximum) step = -1;
+            progressBar1.Value += step;
+        }
+
+        private void T_Tick(object? sender, EventArgs e)
+        {
+            Turn.Draw();
+            Turn.OffsetAngle = Turn.OffsetAngle >= 360 ? 0 : Turn.OffsetAngle + roffset; //如果旋转角度大于360就置0实现
+
+            if (roffset > 0.1)  //当旋转角度小于0.1时就停止
+            {
+                roffset = roffset - roffset / 100;  //减速的幅度逐渐减小
+            }
+            else
+            {
+                roffset = 0;
+                t.Stop();
+                System.Media.SystemSounds.Beep.Play(); //转盘停止提示音
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_MouseDown(object sender, MouseEventArgs e)
+        {
+            progressBar1.Value = progressBar1.Minimum;
+            t1.Interval = 30; //定时器t1的间隔设置为30毫秒
+            t1.Start();
+        }
+
+        private void BtnStart_MouseUp(object sender, MouseEventArgs e)
+        {
+            t1.Stop();
+            roffset = progressBar1.Value;  //进程条的进度赋给偏移量
+            t.Interval = 1;  //定时器t的间隔设置为1毫秒
+            t.Start();
+        }
+
+        private void richTextBox1_Validated(object sender, EventArgs e)
+        {
+        }
+
+        private void BtnReady_Click(object sender, EventArgs e)
+        {
+            string[] strings = richTextBox1.Text.Split('\n'); //将richTextBox1的文本内容以回车键为切割点 赋值给strings
+            List<string> list = new();
+            foreach (var name in strings)
+            {
+                list.Add(name);
+            }
+            Turn.Items = list; //将strings中的字符串赋给转盘中的文字提示
+            Turn.Draw();
+
+
+            //绘制一个指针三角
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);//窗口句柄
+            Brush brush = new SolidBrush(Color.Black);
+            g.FillPolygon(brush, new Point[] { new Point(Turn.Diameter, Turn.Radius), new Point(Turn.Diameter + 60, Turn.Radius + 20), new Point(Turn.Diameter + 60, Turn.Radius - 20) });
+
+        }
+
+   
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TurnTable-master/Form5.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace TurnTableDemo
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label3 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("微软雅黑", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(33, 41);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 31);
+            label1.TabIndex = 0;
+            label1.Text = "订单";
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(24, 24);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("华文中宋", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(40, 104);
+            textBox1.Margin = new Padding(4);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(508, 339);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(392, 477);
+            button1.Margin = new Padding(4);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 56);
+            button1.TabIndex = 3;
+            button1.Text = "提交订单";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("黑体", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(40, 477);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(82, 24);
+            label3.TabIndex = 5;
+            label3.Text = "label3";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(202, 477);
+            button2.Name = "button2";
+            button2.Size = new Size(158, 56);
+            button2.TabIndex = 6;
+            button2.Text = "返回修改订单";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(11F, 24F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 600);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Margin = new Padding(4);
+            Name = "Form5";
+            Text = "订单显示";
+            Load += Form5_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private Button button2;
+    }
+}
